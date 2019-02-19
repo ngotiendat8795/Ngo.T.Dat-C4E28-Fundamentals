a00f2fddddeab6affc35727bae97de4d3fd0c309
ngotiendat8795 <ngotiendat8795@gmail.com>
2019-02-19 22:06:19 +0700

    bla bla

---
+++ Session 1/Intro.py
@@ -0,0 +1,4 @@
+x = int(input("Nhập năm sinh của mày vào: "))
+y = int(input("Nhap năm nay vào: "))
+age = y - x
+print(age)
---
+++ Session 1/turtle_intro.py
@@ -0,0 +1,35 @@
+from turtle import *
+speed(0)
+shape('turtle')
+# for i in range(200):
+
+#     for h in range (3):
+#      forward(100)
+#      left(90)
+#     forward(100)
+
+#     left(8)
+
+# for j in range(200):
+
+#     for i in range(4):
+
+#         forward(100)
+#         penup()
+#         forward(100)
+#         pendown()
+#         left(90)
+#     left(7)
+
+
+circle(50)
+mainloop()
+
+mainloop()
+# color("red","yellow")
+# begin_fill()
+# for i in range(4):
+#     forward(100)
+#     left(90)
+# end_fill()
+
